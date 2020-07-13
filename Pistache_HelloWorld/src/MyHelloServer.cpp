9f1f04355ab0bc148b4e65e5ceb68e775c443e06
ashkhi <ashkhi@gmail.com>
2020-07-13 07:51:47 +0530

    Cnostructor and destructor additions for object init sequence idea

--- Pistache_HelloWorld/src/MyHelloServer.cpp
+++ Pistache_HelloWorld/src/MyHelloServer.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
 #include "MyHelloServer.h"
+
+MyHelloServer::MyHelloServer()
+{
+    std::cout << "\nMyHelloServer instance constructed" << std::endl;
+}
+
+MyHelloServer::~MyHelloServer()
+{
+    std::cout << "\nMyHelloServer instance destroyed" << std::endl;
+}
 
 void MyHelloServer::onRequest(const Http::Request& request, Http::ResponseWriter response)
 {
